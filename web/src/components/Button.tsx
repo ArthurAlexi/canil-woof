f63fbed6944c09564045494059d3ef69e71c1af5
Arthur Alexi <90854173+ArthurAlexi@users.noreply.github.com>
2024-05-07 20:17:47 -0300

    feat: add Button component

---
+++ web/src/components/Button.tsx
@@ -0,0 +1,11 @@
+import { ComponentProps } from "react";
+
+interface ButtonProps extends ComponentProps<'button'>{
+
+}
+
+export function Button({...props} : ButtonProps){
+    return (
+        <button  {...props} className='p-2 bg-slate-700 uppercase rounded-sm hover:bg-slate-300 hover:text-slate-700' />
+    )
+}
