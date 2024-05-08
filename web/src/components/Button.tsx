b5213d81233168bc7c2f2acb426623901aa1dfca
Arthur Alexi <90854173+ArthurAlexi@users.noreply.github.com>
2024-05-08 20:45:02 -0300

    style: change the  border collors

--- web/src/components/Button.tsx
+++ web/src/components/Button.tsx
@@ -6,6 +6,6 @@ interface ButtonProps extends ComponentProps<'button'>{
 
 export function Button({...props} : ButtonProps){
     return (
-        <button  {...props} className='p-2 bg-slate-700 uppercase rounded-sm hover:bg-slate-300 hover:text-slate-700' />
+        <button  {...props} className='p-2 bg-slate-700 uppercase rounded-sm hover:bg-slate-300 hover:text-slate-700 transition-colors' />
     )
 }
--- web/src/components/card.tsx
+++ web/src/components/card.tsx
@@ -14,7 +14,7 @@ export function Card({ petStore, action }: CardProps) {
                 <img src={logo} alt='Canil WOOF' className='w-[50px] h-auto' />
                 <h2 className='font-bold text-2xl'>Resultado</h2>
             </div>
-            <div className='p-3 border rounded-md my-2'>
+            <div className='p-3 border-2 rounded-md my-2 border-slate-900'>
                 <h2 className=' text-xl text-slate-900'>PetShop: <span className='font-semibold ml-4'>{petStore?.name}</span> </h2>
                 <h2 className=' text-xl text-slate-900'>Distância: <span className='font-semibold ml-3'> {petStore?.distance} KM </span> </h2>
                 <h3 className=' text-xl text-slate-900'>Valor: <span className='font-semibold ml-12'> {petStore?.totalCost.toLocaleString('pt-br', {
