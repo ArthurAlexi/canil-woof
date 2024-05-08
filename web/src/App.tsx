96ee301be7bae2fa6cc6e74588ddb3a51a2e38a4
Arthur Alexi <90854173+ArthurAlexi@users.noreply.github.com>
2024-05-08 19:37:06 -0300

    refactor: rename files imports

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css'
 import logo from '../public/logo.png'
-import { Button } from './components/Button'
-import Input from './components/Input'
+import { Button } from './components/button'
+import Input from './components/input'
 import { Header } from './components/header'
 
 import { useForm, SubmitHandler } from "react-hook-form"
