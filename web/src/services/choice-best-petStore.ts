8ba7622e538742fa1969707b5db9ed2af5859a77
Arthur Alexi <90854173+ArthurAlexi@users.noreply.github.com>
2024-05-08 18:48:33 -0300

    feat: use axios to Requests

--- web/src/services/choice-best-petStore.ts
+++ web/src/services/choice-best-petStore.ts
@@ -1,12 +1,10 @@
+import { api } from "../libs/api"
 import { PetStore } from "../models/PetStore"
 import { RequestPetStore } from "../models/RequestPetStore"
 
 export const choiceBestPetShop = async ( body: RequestPetStore): Promise<PetStore> => {
-    const result = await fetch('http://localhost:8080/api/v1/petstore', {
-        method: "POST",
-        body
-    })
-
-    const data = await result.json()
-    return data as PetStore
+    
+    const result = await api.post('petstore/choice', body)
+    
+    return result.data as PetStore
 }
